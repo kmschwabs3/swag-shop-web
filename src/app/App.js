884db3c765ac7819e8a57d8e512dda145ee7f726
Krissy <35669945+kmschwabs3@users.noreply.github.com>
2019-02-17 13:53:32 -0600

    updated to connect to swag-shop API

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HttpService from '../services/http-service-js/http-service';
+ 
+const http = new HttpService();
 
 class App extends Component {
+
+  constructor (props){
+    super (props);
+    http.getProducts();    
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome to the Swag Shop!
           </p>
           <a
             className="App-link"
